215393ee54e64d3f1297dd1d924c9e2f5ffbc8dd
CondoriM <condoria.miguel@gmail.com>
2022-04-12 16:47:54 +0200

    section post commit

---
+++ assets/main.js
@@ -0,0 +1,54 @@
+const posts = [
+    {
+        id: 120394,
+        name_user: 'marcoM',
+        foto_user: 'user',
+        date: 'mm-gg-yyyy',
+        text: 'text post',
+        likes: 12,
+    },
+    {
+        id: 120394,
+        name_user: 'marcoM',
+        foto_user: 'user',
+        date: 'mm-gg-yyyy',
+        text: 'text post',
+        likes: 12,
+    },
+    {
+        id: 120394,
+        name_user: 'marcoM',
+        foto_user: 'user',
+        date: 'mm-gg-yyyy',
+        text: 'text post',
+        likes: 12,
+    }
+]
+
+const postArea = document.querySelector('post_area')
+console.log(postArea);
+
+posts.forEach((post) => {
+    let postSection = `
+    <div class="post">
+        <div class="head_post">
+            <div class="icon_logo">${post.foto_user}</div>
+            <span class="post_user">${post.name_user}</span>
+            <div class="post_date">${post.date}</div>
+        </div>
+        <div class="main_post">
+            <p class="text_post">${post.text}</p>
+            <div class="img_post">${post.foto_user}</div>
+        </div>
+        <div class="foot_post">
+            <input type="button">
+            <div class="like_post">${post.likes}</div>
+        </div>
+    </div>
+    `
+    postArea.append(postSection)
+})
+
+function createPostArea(){
+    
+}
